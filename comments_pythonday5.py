8c02bbc87c8d77592e8923e9350853cceb2df075
maxallday <you@example.com>
2024-05-19 01:44:50 +0300

    Initial commit

---
+++ comments_pythonday5.py
@@ -0,0 +1,28 @@
+#this is a singlr linr comment
+print("Hello World")    
+
+''' 
+This is a multi-line comment. It spans several lines and is used for more extensive explanations. 
+''' 
+print("Hello again!")
+
+'''
+Indentation: Unlike many languages, 
+Python uses indentation to indicate blocks of code. 
+Stick to 4 spaces or a tab for indentation.
+
+if True: 
+    print("This line is indented.") # Correct indentation
+   
+Variable Naming: Use descriptive names for your variables
+user_name = "John" # Good 
+x = "John" # Bad
+
+Whitespace: Use spaces around operators for readability
+# Good 
+total = 5 + 3 
+
+# Bad 
+total=5+3
+'''
+
---
+++ operators_pythonday6.py
@@ -0,0 +1,11 @@
+#additional operators
+#print (round(5/4, 2))
+#print(5>19 or 10>1)
+age = input("How old are you?")
+int(age)
+if age > 18:
+    print("You are old enough to vote!")
+    print("Please put your vote in.")
+else:
+    print("Sorry, you are too young to vote.")
+    print("Please register to vote as soon as you turn 18!")
