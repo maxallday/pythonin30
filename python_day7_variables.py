72771f71bf926f3dcc510135b8929a87cff52817
maxallday <you@example.com>
2024-05-20 16:05:00 +0300

    python_day7_variables day 7

---
+++ python_day7_variables.py
@@ -0,0 +1,12 @@
+#Assigning an integer value to a variable
+age = 21
+# Assigning a string value to a variable
+name = "Joie"
+
+color = "purple"
+# using variables to print
+
+
+print("Elle name is "+name+" and she is "+str(age)+" years old and her favorite color is " + color+".")
+
+
