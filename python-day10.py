8c856549ea487435968cccc6e459204d4cf92cbb
maxallday <you@example.com>
2024-05-23 14:52:46 +0300

    commit day 10, for loops

---
+++ python-day10.py
@@ -0,0 +1,21 @@
+#🐍 Day 10: Automate the Boring Stuff with 'For Loops' 🐍
+'''
+range = [1, 2, 3, 4, 5]
+for x in range:
+    print(f"the square of {x} is, {x**2}")
+'''
+'''
+Homework: Your Turn to Loop
+Loop through a list: Create a list of your five favorite fruits. Use a for loop to print each one out.
+Number Loop: Create a list containing the numbers 1 through 10. Use a for loop to print the cube of each number.'''
+''''''
+# Homework: Your Turn to Loop
+
+#fruits = ["apple 🍎", "banana 🍌", "Peaches 🍑", "Oranges 🍊", "mango 🥭", "Strawberry🍓"]
+#for list in fruits:
+    #print(list)
+
+#Number Loop: Create a list containing the numbers 1 through 10. Use a for loop to print the cube of each number.
+numb =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for num in numb:
+    print(num**3)
