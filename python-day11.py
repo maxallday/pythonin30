b33dba9a86dd8cd4b37d7c50118a743a9df3d324
maxallday <you@example.com>
2024-05-24 11:10:29 +0300

    python safari day 11, while loops
    Homework: Dive Into while Loops
    Counter Challenge: Write a while loop that counts from 1 to 10.

---
+++ python-day11.py
@@ -0,0 +1,18 @@
+#🐍 Day 11: Keep Going with ‘While Loops’ 🐍
+count = 88
+while count <= 100:
+    if count % 2 == 0:
+        print(f"even number {count}")
+    else:
+        print(f"odd number {count}")
+    count += 1
+'''
+Homework: Dive Into while Loops
+Counter Challenge: Write a while loop that counts from 1 to 10.
+'''
+#Counter Challenge: Write a while loop that counts from 1 to 10.
+
+count = 10
+while count <=  10:
+    print(f"number {count}")
+    count += 1  #count = count + 1
